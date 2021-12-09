fa6f16d03f258f3ac6a0bf3e5fbc835210fc651f
manuel_saavedra <manueljss@live.com>
2021-12-09 00:40:37 -0400

    Creacion del primer modelo de datos

---
+++ Infrastructure/Repositories/Impl/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Solicitud_Fondos_Avance_API.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Solicitud_Fondos_Avance_API.Infrastructure.Repositories.Impl
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : class
+    {
+
+
+        public Task<bool> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Upsert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Migrations/20211209023749_initialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Solicitud_Fondos_Avance_API.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    apPaterno = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    apMaterno = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cargo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    rol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pass = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    estado = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
